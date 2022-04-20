ec58f651358bd8c82a3f2ee7c39600b6bdf6dbca
tks3141 <28818747+tks3141@users.noreply.github.com>
2022-04-20 19:29:41 +0900

    line chat ふうになった

--- lib/content.ts
+++ lib/content.ts
@@ -1,5 +1,29 @@
 export type Content = string
 export type Contents = string[]
 
-export const form_data =
-  [{ sheet_id: '1phjOtK0SocSdtu0TxM7dVAd6AEWFOegH8QDeMBSOY4E' }]
+export type Day = string
+export type SheetId = string
+
+type FormData = {
+  sheet_id: SheetId,
+  day: Day,
+}
+
+
+export const form_datas: FormData[] =
+  [{ sheet_id: '1phjOtK0SocSdtu0TxM7dVAd6AEWFOegH8QDeMBSOY4E', day: '2022-04-15' }]
+
+export class ContentModel {
+  data = form_datas;
+  sheet_ids: { [key: Day]: SheetId } = {};
+
+  constructor() {
+    this.data.forEach(e => {
+      this.sheet_ids[e.day] = e.sheet_id;
+    });
+  }
+
+  get_sheet_id = (day: Day) => {
+    return this.sheet_ids[day];
+  }
+}
---
+++ pages/day/[day].tsx
@@ -0,0 +1,64 @@
+// import styles from '../styles/Home.module.css';
+import { Content, ContentModel } from '../../lib/content';
+import { getContents } from '../../lib/spreadsheet';
+import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { Stack, Box } from '@mui/material';
+import { ParsedUrlQuery } from 'querystring';
+
+import { Day } from '../../lib/content';
+import styles from './[day].module.css';
+
+
+type Props = { contents: Content[] }
+interface Params extends ParsedUrlQuery {
+	day: Day,
+}
+
+const model = new ContentModel();
+
+
+const LineChat = (props: { comments: string[], title:string}) => {
+	return (
+		<Container maxWidth="md" className={styles.main}>
+			<Box className={styles.header}>{props.title}（{props.comments.length}人）</Box>
+			<Stack spacing={0}>
+				{props.comments.map(c =>
+					<Box key={c} className={styles.wrapper}>
+						<span className={styles.icon}></span>
+						<div className={styles.comment}>{c}</div>
+					</Box>)
+				}
+			</Stack>
+		</Container>
+	)
+}
+
+export default function DailyResults({ contents }: Props) {
+	return (
+		<LineChat comments={contents} title="実践的機械学習Ⅰ 2022"/>
+	)
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (context) => {
+	const { day } = context.params!;
+	const sheet_id = model.get_sheet_id(day);
+	const contents = await getContents(sheet_id);
+	return {
+		props: { contents },
+		revalidate: 3600,
+	};
+}
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+	const paths = model.data.map(d => ({
+		params: { day: d.day }
+	}))
+	console.log(paths)
+	return {
+		paths: paths,
+		fallback: false
+	}
+}
