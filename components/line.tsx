64cfb0b8daa497a1e110628720f7655ab0d7a9d9
tks3141 <28818747+tks3141@users.noreply.github.com>
2022-07-08 16:40:38 +0900

    右寄せの表示を改善

--- components/line.tsx
+++ components/line.tsx
@@ -12,13 +12,14 @@ export function LinePop(props: LineComment) {
   if (props.pos == 'right') {
     // pos right
     return (
+      // right allign
       <Box className={styles.teacher}>
         <div className={styles.icon}></div>
         <div className={styles.wrapper}>
           <div className={styles.name}>先生</div>
         </div>
-        <div className={styles.wrapper}>
-          <div className={styles.comment}>{props.message}</div>
+        <div className={styles.right_wrapper}>
+          <div className={styles.comment}><div className={styles.left_wrapper}>{props.message}</div></div>
           {/* <div> ○月○日○時○分○</div> */}
         </div>
       </Box>
