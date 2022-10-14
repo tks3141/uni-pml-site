48f895dba631f402eec51d5fe5987145c543b277
tks3141 <28818747+tks3141@users.noreply.github.com>
2022-10-14 22:11:52 +0900

    safe

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -3,12 +3,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Content } from '../../lib/content';
 import { getContents } from "../../lib/spreadsheet";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any[]>
-) {
-  getContents(process.env.SPREADSHEET_ID as string).then((d) => {
-    console.log(d)
-    return res.status(200).json(d)
-  })
-}
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<any[]>
+// ) {
+//   getContents(process.env.SPREADSHEET_ID as string).then((d) => {
+//     console.log(d)
+//     return res.status(200).json(d)
+//   })
+// }
